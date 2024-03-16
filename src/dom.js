72457fef0a134522f52732166349070f1a97bada
Kris <109919530+maleker86@users.noreply.github.com>
2024-03-16 12:16:25 -0700

    this.task in dom.js is not a function

--- src/dom.js
+++ src/dom.js
@@ -8,6 +8,30 @@ proj_button.addEventListener("click", function () {
   console.log("I am a project");
 });
 
+function newTask(task_title,priority_string) {
+  this.task_title = task_title;
+  this.priority_string = priority_string;
+} 
+
+function newProject(project_title,task_title,priority_string) {
+  const items = [
+    this.task_title = task_title,
+    this.priority_string = priority_string,
+    this.project_title = project_title,
+  ]
+  (function pushItems(items) {
+    createdItems.push(items);
+  })();
+}
+
+let createdItems = []
+console.log(createdItems);
+let obj1 = new newTask("get up",null);
+let obj2 = new newProject('program','finish project',"medium");
+let obj3 = new newTask('third',"high priority");
+let obj4 = new newProject('do dishes',null,"high");
+let obj5 = new newProject('do laundry','finish chores',"low");
+
 export function displayItems(array) {
   console.log(array);
   array.forEach((element) => {
@@ -15,22 +39,29 @@ export function displayItems(array) {
     let h2 = document.createElement("h2");
     document.body.append(container);
 
-    if (element.name) {
+    if (element.project_title) {
+      // console.log("I am a project!");
+      let h2 = document.createElement("h2");
       container.append(h2);
-      h2.innerText = "Name is: " + element.name;
-    }
-
-    if (element.project) {
-      console.log("I am a project!");
+      h2.innerText = "This is a project called: " + element.project_title;
+    } 
+    
+    if (element.project_title && element.task_title) {
       let p = document.createElement("p");
       container.append(p);
-      p.innerText = "This is a project.";
+      p.innerText = "Task title is: " + element.task_title;
     }
 
-    if (element.priority) {
+    if (!element.project_title && element.task_title) {
+      container.append(h2);
+      h2.innerText = "Task title is: " + element.task_title;
+    }
+
+
+    if (element.priority_string) {
       let p = document.createElement("p");
       container.append(p);
-      p.innerText = element.priority;
+      p.innerText = element.priority_string;
     }
 
     // p.innerText = "testing";
--- src/storage.js
+++ src/storage.js
@@ -1,8 +1,20 @@
 let test_arr = [
-    {name: 'first', project: false,},
-    {name: 'second', project: true, priority: 'low',},
-    {name: 'third', priority: "high priority"},
 ];
+
+let obj1 = new newTask(null,"get up",null);
+let obj2 = new newTask('program','finish project',"medium");
+let obj3 = new newTask(null,'third',"high priority");
+let obj4 = new newTask('do dishes',null,"high");
+let obj5 = new newTask('do laundry','finish chores',"low");
+
+function newTask(project_title,task_title,priority_string) {
+    this.task_title = task_title;
+    this.project_title = project_title;
+    this.priority_string = priority_string;
+}
+
+
+test_arr.push(obj1,obj2,obj3,obj4,obj5);
 
 export {
     test_arr,
